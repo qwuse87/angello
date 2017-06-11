7e13e629f595dfee9e846163182118ff05e0f7af
Leesuyun <qwuse87@gmail.com>
2017-06-11 12:08:16 +0900

    안젤로 라이트 버젼 세팅

---
+++ angello-lite/js/app.js
@@ -0,0 +1,5 @@
+var myModule = angular.module('Angello', []);
+myModule.factory('AngelloHelper', function(){ });
+myModule.service('AngelloModel', function(){ });
+myModule.controller('MainCtrl', function(){ });
+myModule.directive('story', function(){ });
